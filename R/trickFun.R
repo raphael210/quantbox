c0a8f68eb986bacfb67721011f87774ea1ef6ce1
QuantAndrew <taoaming1001@163.com>
2016-09-12 10:57:59 +0800

    add laddernav function

--- R/trickFun.R
+++ R/trickFun.R
@@ -5,6 +5,17 @@
 "portdemo"
 
 
+#' assets return demo dataset.
+#'
+#' A dataset containing stock index and bond index daily return data since 2009.
+#'
+#' @format A data frame with 1627 rows and 3 variables:
+#' \describe{
+#'   \item{date}{date type}
+#'   \item{stock}{stock index return}
+#'   \item{bond}{bond index return}
+#' }
+"rtndemo"
 
 
 #' bracket a series of string
@@ -140,5 +151,57 @@ db.ts <- function(){
 }
 
 
+
+#' ladder position port function
+#'
+#' @param assetRtn a data frame for stock and bond return.
+#' @param ruledf a data frame for position rule.
+#' @param rebalance rebalance frequency, default value is NULL.
+#' @return a data frame, containing return,postion,nav.
+#' @author han.qian
+#' @export
+#' @examples
+#' assetRtn <- rtndemo
+#' ruledf <- data.frame(nav = c(0,1.05,1.10,1.20),pos = c(0.15,0.25,0.35,0.5))
+#' re <- ladderNAV(assetRtn,ruledf)
+#' rebalance <- '2 years'
+#' re <- ladderNAV(assetRtn,ruledf,rebalance)
+ladderNAV <- function(assetRtn,ruledf,rebalance=NULL){
+  assetRtn[,c('pos','rtn','nav_rebalance','nav')] <- 0
+
+  if(is.null(rebalance)){
+    for(i in 1:nrow(assetRtn)){
+      if(i==1){
+        assetRtn$pos[i] <- ruledf$pos[1]
+      }else{
+        postmp <- findInterval(assetRtn$nav_rebalance[i-1],ruledf$nav)
+        assetRtn$pos[i] <- ruledf$pos[postmp]
+      }
+      assetRtn$rtn[i] <- assetRtn$stock[i]*assetRtn$pos[i]+assetRtn$bond[i]*(1-assetRtn$pos[i])
+      assetRtn$nav_rebalance[i] <- prod(1+assetRtn$rtn[1:i])
+      assetRtn$nav[i] <- prod(1+assetRtn$rtn[1:i])
+    }
+  }else{
+    reDate <- seq.Date(min(assetRtn$date),max(assetRtn$date),by = rebalance)
+    reDate <- QDataGet::trday.nearby(reDate,by=0)
+    for( i in 1:nrow(assetRtn)){
+      if(assetRtn$date[i] %in% reDate){
+        assetRtn$pos[i] <- ruledf$pos[1]
+        j <- i
+      }else{
+        postmp <- findInterval(assetRtn$nav_rebalance[i-1],ruledf$nav)
+        assetRtn$pos[i] <- ruledf$pos[postmp]
+      }
+      assetRtn$rtn[i] <- assetRtn$stock[i]*assetRtn$pos[i]+assetRtn$bond[i]*(1-assetRtn$pos[i])
+      assetRtn$nav_rebalance[i] <- prod(1+assetRtn$rtn[j:i])
+      assetRtn$nav[i] <- prod(1+assetRtn$rtn[1:i])
+    }
+  }
+
+  if(is.null(rebalance)){
+    assetRtn <- assetRtn[,c("date","stock","bond","pos","rtn","nav")]
+  }
+  return(assetRtn)
+}
 
 
